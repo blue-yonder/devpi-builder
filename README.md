5ae071cb17bd316394559d9e891941b135fe9bb0
Matthias Bach <matthias.bach@blue-yonder.com>
2014-07-25 11:55:08 +0200

    Initial project description

---
+++ README.md
@@ -0,0 +1,23 @@
+Brandon the Builder
+===================
+
+This tools takes a list of python packages, specified as in an `requirements.txt` and incrementally fills a
+[devpi](http://doc.devpi.net/latest/) index with them.
+
+“Features”
+----------
+
+Please not that this are only planned yet.
+
+ * Reads a `requirements.txt` stile input file.
+ * Multiple versions of a package may be imported.
+ * Only builds packages not yet in the target index.
+ * Can use separate indices for plain python packages and those with binary contents.
+    - Optionally only operates on one of the two.
+ * Supports a black-list for packages to never be built and uploaded (certain packages like numpy are fragile regarding
+   their interdependency with other packages).
+
+License
+-------
+
+It would be neat to keep this package clean, thus that maybe later it could be put under an open-source license.
---
+++ setup.py
@@ -0,0 +1,33 @@
+__author__ = 'mbach'
+
+import multiprocessing # avoid crash on teardown
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'Brandon',
+    version = '0.1-dev',
+    packages = find_packages(),
+    author='Matthias Bach',
+    author_email='matthias.bach@blue-yonder.com',
+    description='Fill in index with wheels from an requirements.txt-like specification file.',
+    license='Proprietary',
+    install_requires=[
+        'setuptools'
+    ],
+    setup_requires=[
+        'nose>=1.0'
+    ],
+    tests_require=[
+        'nose>=1.0'
+    ],
+    test_suite='nose.collector',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: Other/Proprietary License',
+        'Topic :: System :: Archiving :: Packaging'
+    ]
+)
