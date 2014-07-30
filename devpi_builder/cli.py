ed6f34ebc682227c9794388972df74744fe23384
Stephan Erb <stephan.erb@blue-yonder.com>
2014-07-30 22:03:42 +0200

    Use the new upload feature

--- devpi_builder/cli.py
+++ devpi_builder/cli.py
@@ -14,11 +14,14 @@ def main(args=None):
     parser = argparse.ArgumentParser(description='Create wheels for all given project versions and upload them to the given index.')
     parser.add_argument('requirements', help='requirements.txt style file specifying which project versions to package.')
     parser.add_argument('index', help='The index to upload the packaged software to.')
+    parser.add_argument('user', help='The user to log in as.')
+    parser.add_argument('password', help='Password of the user.')
 
     args = parser.parse_args(args=args)
 
-    with wheeler.Builder() as builder, devpi.Client(args.index) as devpi_client:
+    with wheeler.Builder() as builder, devpi.Client(args.index, args.user, args.password) as devpi_client:
         for (package, version) in requirements.read(args.requirements):
             if not devpi_client.package_version_exists(package, version):
                 print 'Building {} {}'.format(package, version)
                 wheel_file = builder(package, version)
+                devpi_client.upload(wheel_file)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,9 +10,10 @@ from tests.tools import devpi_server, devpi_index
 
 class CliTest(unittest.TestCase):
     def test_basic(self):
-        with devpi_server() as server_url, devpi_index(server_url, 'test', 'wheels') as (destination_index, password):
+        user = 'test'
+        with devpi_server() as server_url, devpi_index(server_url, user, 'wheels') as (destination_index, password):
 
-            main(['tests/sample_simple.txt', destination_index])
+            main(['tests/sample_simple.txt', destination_index, user, password])
 
             with devpi.Client(destination_index) as devpi_client:
                 self.assert_(devpi_client.package_version_exists('progressbar', '2.2'))
