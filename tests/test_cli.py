2e9808cc6c25a663243dd652eb1388e440ae0b18
Matthias Bach <matthias.bach@blue-yonder.com>
2014-08-01 14:50:25 +0200

    Test building multiple versions of a package in one run.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,7 +16,7 @@ class CliTest(unittest.TestCase):
             main(['tests/sample_simple.txt', destination_index, user, password])
 
             with devpi.Client(destination_index) as devpi_client:
-                self.assert_(devpi_client.package_version_exists('progressbar', '2.2'))
+                self.assertTrue(devpi_client.package_version_exists('progressbar', '2.2'))
 
     def test_with_blacklist(self):
         user = 'test'
@@ -28,6 +28,15 @@ class CliTest(unittest.TestCase):
                 self.assertFalse(devpi_client.package_version_exists('progressbar', '2.2'))
                 self.assertTrue(devpi_client.package_version_exists('six', '1.7.3'))
 
+    def test_multiple_versions(self):
+        user = 'test'
+        with devpi_server() as server_url, devpi_index(server_url, user, 'wheels') as (destination_index, password):
+
+            main(['tests/sample_multiple_versions.txt', destination_index, user, password])
+
+            with devpi.Client(destination_index) as devpi_client:
+                self.assertTrue(devpi_client.package_version_exists('progressbar', '2.1'))
+                self.assertTrue(devpi_client.package_version_exists('progressbar', '2.2'))
 
 
 if __name__ == '__main__':
