d35a5caa59712fe234220d1da2ece0aaeafae9cd
Matthias Bach <matthias.bach@blue-yonder.com>
2014-08-15 13:35:02 +0200

    Bump version to reflect new feature

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='devpi-builder',
-    version='0.2.2.dev',
+    version='0.3.0-dev',
     packages=find_packages(exclude=['tests']),
     author='Matthias Bach',
     author_email='matthias.bach@blue-yonder.com',
