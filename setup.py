7ce99b9f4c5683c24448c2887f389da092eaf3c7
Matthias Bach <matthias.bach@blue-yonder.com>
2014-08-26 17:42:38 +0200

    Add URL to package info.

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     author='Matthias Bach',
     author_email='matthias.bach@blue-yonder.com',
+    url='https://github.com/blue-yonder/devpi-builder',
     description='Devpi-builder takes a requirements.txt and incrementally fills a devpi index with wheels of the listed python packages.',
     long_description=readme,
     license='new BSD',
